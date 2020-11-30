8fc0182d51950e791859ef50064c343fec4089b5
ming luo <itestmycode@gmail.com>
2020-11-30 09:02:26 -0500

    dashboard login page

---
+++ dashboard/src/auth/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" v-model="username">
+        </div>       
+        
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" v-model="password">
+        </div>
+        
+        <button type="button" v-on:click="login">Login</button>
+    </div>
+</template>
+
+<script>
+    import { loginUser } from './auth'
+    export default {
+        name: 'login',
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            async login() {
+                try {
+                    await loginUser(this.username, this.password)
+                    this.$router.push('/')
+                }
+                catch (err) {
+                    alert(`Error: ${err}`)
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .input-group {
+        margin: 1rem;
+    }
+
+    .input-group label {
+        margin-right: 0.5rem;
+    }
+</style>
---
+++ dashboard/src/auth/auth.js
@@ -0,0 +1,69 @@
+import decode from 'jwt-decode'
+import axios from 'axios'
+
+const REST_ENDPOINT = 'http://localhost:6454/'
+const AUTH_TOKEN_KEY = 'authToken'
+
+export function loginUser(username, password) {
+    return new Promise((resolve, reject) => {
+        return axios.post(`${REST_ENDPOINT}api/v1/auth/token`, {
+                username: username,
+                password: password,
+                grant_type: 'password'
+        }).then(response => {
+            setAuthToken(response.data.token)
+            resolve();
+        }).catch (err => {
+            console.error('Caught an error during login:', err)
+            reject(err);
+        });
+    })
+}
+
+export function logoutUser() {
+    clearAuthToken()
+}
+
+export function setAuthToken(token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    console.log('set token', token)
+    localStorage.setItem(AUTH_TOKEN_KEY, token)
+}
+
+export function getAuthToken() {
+    return localStorage.getItem(AUTH_TOKEN_KEY)    
+}
+
+export function clearAuthToken() {
+    axios.defaults.headers.common['Authorization'] = ''
+    localStorage.removeItem(AUTH_TOKEN_KEY)
+}
+
+export function isLoggedIn() {
+    let authToken = getAuthToken()
+    console.log("this is token from local storage", authToken)
+    return !!authToken && !isTokenExpired(authToken)
+}
+
+export function getUserInfo() {
+    if (isLoggedIn()) {
+        return decode(getAuthToken())
+    }
+}
+
+function getTokenExpirationDate(encodedToken) {
+    let token = decode(encodedToken)
+    if (!token.exp) {
+        return null
+    }
+  
+    let date = new Date(0)
+    date.setUTCSeconds(token.exp)
+  
+    return date
+}
+  
+function isTokenExpired(token) {
+    let expirationDate = getTokenExpirationDate(token)
+    return expirationDate < new Date()
+}
