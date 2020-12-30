94fd820d4786e4978027b4f69081706ac856a6dd
ming luo <itestmycode@gmail.com>
2020-12-30 16:44:58 -0500

    copyright

--- dashboard/src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIconVuestic.vue
+++ dashboard/src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIconVuestic.vue
@@ -1,3 +1,20 @@
+<!--
+  Copyright 2021 DataStax, Inc. 
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  
+ -->
+
 <template>
   <svg class="vuestic-icon-vuestic" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 129 15.4" style="enable-background:new 0 0 129 15.4;" xml:space="preserve">
     <g>
