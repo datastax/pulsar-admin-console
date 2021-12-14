0a605276cbc16b0ae0c7f84f1d6d013e90b125c6
Chris Bartholomew <c_bartholomew@yahoo.com>
2021-12-14 15:53:26 -0500

    Fix k8s authentication (#19)

--- dashboard/src/services/ApiBaseV2.js
+++ dashboard/src/services/ApiBaseV2.js
@@ -19,13 +19,21 @@ import axios from 'axios'
 import store from '../store/index'
 
 export default() => {
+
+  let token = ''
+  if (store.getters.authMode === 'k8s') {
+    token = store.getters.adminToken;
+  } else if (store.getters.authMode === 'openidconnect')  {
+    token = store.getters.clientToken;
+  } 
+
   return axios.create({
     baseURL: store.getters.apiBaseUrl.replace('v1', 'v2'),
     withCredentials: false,
     timeout: 4500,
     headers: {
       'Accept': 'application/json',
-      'Authorization': `Bearer ${store.getters.clientToken}`
+      'Authorization': `Bearer ${token}`
     }
   })
 }
